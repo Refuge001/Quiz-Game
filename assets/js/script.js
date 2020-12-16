13a6516e8107fab67c7dbe5f1795294601b9f37a
Will Utterback <uwill08@gmail.com>
2020-12-16 06:14:41 -0600

    Added double-function, and for loop for answer button class array, might not need

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@ var totalSeconds = "";
 var interval = "";
 var secondsDisplay = document.querySelector("#timer");
 var startButton = document.querySelector("#start-btn");
+var submitButton = document.querySelector("#score-btn");
+//getElementsByClassName returns an array
+var answerButton = document.getElementsByClassName("answer");
+var score = [];
+
+var correctAnswer = answerButton[1]; //
+
+const questionArr = ["Question 1?", "Question 2?"];
+const answerArr = ["Answer 1", "Answer 2", "Answer 3", "Answer 4"];
+
 
 function startTimer() {
   interval = "";
@@ -20,4 +30,29 @@ function startTimer() {
   }
 }
 
-startButton.addEventListener("click", startTimer);
+function startQuiz() {
+  alert("Quiz Started");
+}
+
+function doubleStart() {
+  score = "";
+  startTimer();
+  startQuiz();
+}
+
+function submitAnswer() {
+  alert("Answer Submitted")
+};
+//loop required to go through answer button Array adding eventListener
+for (let i = 0; i < answerButton.length; i++) {
+  answerButton[i].addEventListener("click", submitAnswer)
+}
+
+function submitScore() {
+  alert("submit clicked");
+}
+
+startButton.addEventListener("click", doubleStart);
+submitButton.addEventListener("click", submitScore);
+
+//answerButton.addEventListener("click", answerSubmit);
